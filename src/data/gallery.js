9afff1d9a14b95541e000ebc4d2a000cd7d4fca5
Haichao Zhao <haichao@seas.upenn.edu>
2026-02-10 12:19:49 +0800

    update

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -101,18 +101,18 @@ export const GALLERY_DATA = {
       id: 1,
       imageUrl: '/images/gallery/stores/store1.jpg',
       location: '太原市小店区总部',
-      address: '山西省太原市小店区xxx路xxx号',
+      address: '山西省太原市小店区巨鑫国际大厦B座5层',
       description: '荣易达学历提升总部办公区',
       features: ['专业咨询团队', '一对一服务', '学员休息区']
     },
-    // {
-    //   id: 2,
-    //   imageUrl: '/images/gallery/stores/store2.jpg',
-    //   location: '太原市迎泽区分部',
-    //   address: '山西省太原市迎泽区xxx路xxx号',
-    //   description: '迎泽区咨询服务中心',
-    //   features: ['便捷交通', '舒适环境', '免费咨询']
-    // },
+    {
+      id: 2,
+      imageUrl: '/images/gallery/stores/store2.png',
+      location: '营业执照',
+      address: ' ',
+      description: ' ',
+      features: ['合法经营', '诚信服务']
+    },
     // {
     //   id: 3,
     //   imageUrl: '/images/gallery/stores/store3.jpg',
